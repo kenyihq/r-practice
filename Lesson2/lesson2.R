2c3d89ed285a61f8372be1e4e52cd4c087ce0a8c
Kenyi Hancco <kenyihq@gmail.com>
2022-06-19 10:25:35 -0500

    Added select, slice

--- Lesson2/lesson2.R
+++ Lesson2/lesson2.R
@@ -44,12 +44,32 @@ library(help=ggplot2movies) # Verificar la version de ggplot2movies
 
 data(movies) # Cargar el archivo de base de datos
 
+mov <- movies # Guardar el archivo de base de datos en una variable
+mov$title[1000:1005] # Mostrar los primeros 5 registros del archivo de base de datos
 
 #====================
 # 1.1. Comando Select
 #====================
+help(select) # Verificar la ayuda del comando select
+
+mov2 <- select(mov, Pelicula=title, Anio=year, Duracion=length, Rating=rating, Votos=votes) # Seleccionar los campos que se requieren
+mov3 <- select(mov, starts_with("r")) # Seleccionar los registros que comienzan con la letra A
+mov4 <- select(mov, ends_with("a")) # Seleccionar los registros que terminan con la letra A
+mov5 <- select(mov, contains("a")) # Seleccionar los registros que contienen la letra A
+
+#===================
+# 1.2 Comando slice
+#===================
+help(slice) # Verificar la ayuda del comando slice
+
+mov6 <- slice(mov, 1:10) # Seleccionar los primeros 10 registros
+mov7 <- slice_sample(mov, n=10) # Seleccionar los primeros 10 registros de forma aleatoria
 
 
+
+# Practice
+
+select(mov7, Pelicula=title, Anio=year, Duracion=length, Rating=rating, Votos=votes)
 
 
 
